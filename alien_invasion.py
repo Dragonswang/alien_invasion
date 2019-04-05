0ce3fdf2dca4ce5d802007e15f35dd8285540e86
Avan Blaney <godWang77@163.com>
2019-04-06 01:01:15 +0800

    外星人大战

---
+++ alien_invasion.py
@@ -0,0 +1,55 @@
+import sys 
+ 
+import pygame
+from settings import Settings 
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
+from ship import Ship
+from alien import Alien 
+import game_functions as gf 
+from pygame.sprite import Group
+ 
+def run_game():     
+	# 初始化pygame、设置和屏幕对象     
+	pygame.init()
+	ai_settings = Settings() 
+
+	screen = pygame.display.set_mode(         
+		(ai_settings.screen_width, ai_settings.screen_height))      
+	
+	# 创建一艘飞船、一个子弹编组和一个外星人编组    
+	ship = Ship(ai_settings, screen)
+	
+	bullets = Group()
+	aliens = Group()
+	# 创建外星人群    
+	gf.create_fleet(ai_settings, screen, ship, aliens)
+	
+	pygame.display.set_caption("Alien Invasion")
+	# 创建Play按钮   
+	play_button = Button(ai_settings, screen, "Play")
+	
+	# 创建存储游戏统计信息的实例，并创建记分牌    
+	stats = GameStats(ai_settings)
+	sb = Scoreboard(ai_settings, screen, stats) 
+	# 开始游戏的主循环        
+	while True: 
+ 
+		# 监视键盘和鼠标事件       
+		
+		gf.check_events(ai_settings, screen, stats, sb, play_button, ship,             
+			aliens, bullets) 
+		
+		if stats.game_active:
+			ship.update()
+			gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens,                
+				bullets) 
+			gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, 
+				bullets)
+		# 每次循环时都重绘屏幕
+		gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, 
+			bullets, play_button)
+		
+ 
+run_game() 
